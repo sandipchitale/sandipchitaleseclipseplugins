834f666075912da39e0c54fc37e1ce80a1d2b05d
sandipchitale <sandipchitale@47c47cea-daa5-11dd-801b-4930de48a34e>
2009-02-20 08:20:17 +0000

    Display status message.

--- Screenshot/src/screenshot/DesktopArea.java
+++ Screenshot/src/screenshot/DesktopArea.java
@@ -1,10 +1,15 @@
 package screenshot;
 
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.IWorkbenchWindowActionDelegate;
+import org.eclipse.ui.internal.WorkbenchWindow;
+import org.eclipse.ui.progress.UIJob;
 
 public class DesktopArea implements IWorkbenchWindowActionDelegate {
 
@@ -17,9 +22,21 @@ public class DesktopArea implements IWorkbenchWindowActionDelegate {
 	}
 
 	public void run(IAction action) {
-        /* Take the screen shot of rubberbanded area*/
-        Shell shell = window.getShell();
-        Util.processImage(shell, Util.getImage(shell));
+		((WorkbenchWindow) window).getStatusLineManager().setMessage("Screenshot in 5 seconds...");
+		UIJob uiJob = new UIJob("Click!") {
+			public IStatus runInUIThread(IProgressMonitor monitor) {
+				/* Take the screen shot */
+				Shell shell = window.getShell();
+				if (shell == null || shell.isDisposed()) {
+					return Status.CANCEL_STATUS;
+				}
+				Util.processImage(shell, Util.getImage(shell));
+				((WorkbenchWindow) window).getStatusLineManager().setMessage("Screenshot in 5 seconds...Done.");
+				return Status.OK_STATUS;
+			}			
+		};
+		uiJob.setPriority(UIJob.INTERACTIVE);
+		uiJob.schedule(5000);
 	}
 
 	public void selectionChanged(IAction action, ISelection selection) {}
