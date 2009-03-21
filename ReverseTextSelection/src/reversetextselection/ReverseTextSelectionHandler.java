fd93f34be2a1269df0d533ee97b086b09ad68e85
sandipchitale <sandipchitale@47c47cea-daa5-11dd-801b-4930de48a34e>
2009-03-21 16:56:25 +0000

    Make it work inside text editors inside multipage editors also.

--- ReverseTextSelection/src/reversetextselection/ReverseTextSelectionHandler.java
+++ ReverseTextSelection/src/reversetextselection/ReverseTextSelectionHandler.java
@@ -12,6 +12,17 @@ import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.texteditor.ITextEditor;
 
+/**
+ * This command handler moves the text caret to the other end of the selection in a
+ * text editor. Repeated invocation of the command bounces the caret from one
+ * end of selection to the other while keeping the selection intact. This
+ * enables you to extend/contract the selection in either direction. The default
+ * key binding for the Reverse Text Selection command is Alt+Shift+/. You can
+ * change the key binding using Preferences > General > Keys page.
+ * 
+ * @author Sandip Chitale
+ * 
+ */
 public class ReverseTextSelectionHandler extends AbstractHandler {
 
 	public Object execute(ExecutionEvent event) throws ExecutionException {
@@ -20,21 +31,30 @@ public class ReverseTextSelectionHandler extends AbstractHandler {
 			IWorkbenchPage activePage = activeWorkbenchWindow.getActivePage();
 			if (activePage != null) {
 				IWorkbenchPart activeEditor = activePage.getActivePart();
+				StyledText styledText = null;
 				if (activeEditor instanceof ITextEditor) {
 					ITextEditor abstractTextEditor = (ITextEditor) activeEditor;
 					Object adapter = (Control) abstractTextEditor.getAdapter(Control.class);
 					if (adapter instanceof Control) {
 						Control control = (Control) adapter;
 						if (control instanceof StyledText) {
-							StyledText styledText = (StyledText) control;
-							Point selection = styledText.getSelection();
-							int caretOffset = styledText.getCaretOffset();
-							if (caretOffset == selection.x) {
-								styledText.setSelection(selection);
-							} else {
-								styledText.setSelection(selection.y, selection.x);
-							}
+							styledText = (StyledText) control;
+
 						}
+					}
+				} else {
+					Control focusControl = activeWorkbenchWindow.getShell().getDisplay().getFocusControl();
+					if (focusControl instanceof StyledText) {
+						styledText = (StyledText) focusControl;
+					}
+				}
+				if (styledText != null) {
+					Point selection = styledText.getSelection();
+					int caretOffset = styledText.getCaretOffset();
+					if (caretOffset == selection.x) {
+						styledText.setSelection(selection);
+					} else {
+						styledText.setSelection(selection.y, selection.x);
 					}
 				}
 			}
@@ -52,16 +72,26 @@ public class ReverseTextSelectionHandler extends AbstractHandler {
 			IWorkbenchPage activePage = activeWorkbenchWindow.getActivePage();
 			if (activePage != null) {
 				IWorkbenchPart activeEditor = activePage.getActivePart();
-				if (activeEditor instanceof ITextEditor) {
-					ITextEditor abstractTextEditor = (ITextEditor) activeEditor;
-					Object adapter = (Control) abstractTextEditor.getAdapter(Control.class);
-					if (adapter instanceof Control) {
-						Control control = (Control) adapter;
-						if (control instanceof StyledText) {
-							StyledText styledText = (StyledText) control;
-							Point selection = styledText.getSelection();
-							return (selection.x != selection.y);
+				if (activeEditor != null) {
+					StyledText styledText = null;
+					if (activeEditor instanceof ITextEditor) {
+						ITextEditor abstractTextEditor = (ITextEditor) activeEditor;
+						Object adapter = (Control) abstractTextEditor.getAdapter(Control.class);
+						if (adapter instanceof Control) {
+							Control control = (Control) adapter;
+							if (control instanceof StyledText) {
+								styledText = (StyledText) control;
+							}
 						}
+					} else {
+						Control focusControl = activeWorkbenchWindow.getShell().getDisplay().getFocusControl();
+						if (focusControl instanceof StyledText) {
+							styledText = (StyledText) focusControl;
+						}
+					}
+					if (styledText != null) {
+						Point selection = styledText.getSelection();
+						return (selection.x != selection.y);
 					}
 				}
 			}
