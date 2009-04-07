3e0b54731a78b6e08e142c06b638377e6d99f2f6
sandipchitale <sandipchitale@47c47cea-daa5-11dd-801b-4930de48a34e>
2009-04-07 04:02:48 +0000

    Cursors

--- trunk/MoveResize/src/moveresize/MoveResize.java
+++ trunk/MoveResize/src/moveresize/MoveResize.java
@@ -5,8 +5,10 @@ import org.eclipse.swt.events.KeyEvent;
 import org.eclipse.swt.events.KeyListener;
 import org.eclipse.swt.events.PaintEvent;
 import org.eclipse.swt.events.PaintListener;
+import org.eclipse.swt.graphics.Cursor;
 import org.eclipse.swt.graphics.GC;
 import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.graphics.ImageData;
 import org.eclipse.swt.graphics.Rectangle;
 import org.eclipse.swt.widgets.Canvas;
 import org.eclipse.swt.widgets.Control;
@@ -21,6 +23,7 @@ class MoveResize {
 	static Rectangle to(Rectangle bounds, final MODE mode) {
 		final Rectangle[] rectangle = new Rectangle[1];
 		Display display = new Display();
+		initCursors(display);
 		final Shell shell = new Shell(display, SWT.NO_TRIM);
 		shell.setBounds(display.getBounds());
 		/* Take the screen shot */
@@ -44,6 +47,9 @@ class MoveResize {
 				}
 			}
 		});
+
+		canvas.setToolTipText("Drag borders to resize. Drag window to move.");
+		
         shell.addKeyListener(new KeyListener() {
 			public void keyPressed(KeyEvent e) {}
 			public void keyReleased(KeyEvent e) {
@@ -81,6 +87,56 @@ class MoveResize {
 	        new Mover(control).attach();
 			break;
 		}
+	}
+	
+	static Cursor getCursor(int c) {
+		switch (c) {
+		case SWT.CURSOR_ARROW:
+			return CURSOR_ARROW;
+		case SWT.CURSOR_SIZEN:
+			return CURSOR_N;
+		case SWT.CURSOR_SIZEE:
+			return CURSOR_E;
+		case SWT.CURSOR_SIZEW:
+			return CURSOR_W;
+		case SWT.CURSOR_SIZES:
+			return CURSOR_S;
+		case SWT.CURSOR_SIZENE:
+			return CURSOR_NE;
+		case SWT.CURSOR_SIZENW:
+			return CURSOR_NW;
+		case SWT.CURSOR_SIZESE:
+			return CURSOR_SE;
+		case SWT.CURSOR_SIZESW:
+			return CURSOR_SW;
+		case SWT.CURSOR_SIZEALL:
+			return CURSOR_MOVE;
+		}
+		return CURSOR_ARROW;
+	}
+	
+	private static Cursor CURSOR_ARROW = null;
+	private static Cursor CURSOR_MOVE = null;
+	private static Cursor CURSOR_N = null;
+	private static Cursor CURSOR_E = null;
+	private static Cursor CURSOR_W = null;
+	private static Cursor CURSOR_S = null;
+	private static Cursor CURSOR_NE = null;
+	private static Cursor CURSOR_NW = null;
+	private static Cursor CURSOR_SE = null;
+	private static Cursor CURSOR_SW = null;
+	
+	private static void initCursors(Display display) {
+		CURSOR_ARROW = display.getSystemCursor(SWT.CURSOR_ARROW);
+		CURSOR_MOVE = new Cursor(display, new ImageData(MoveResize.class.getResourceAsStream("CURSOR_MOVE.png")), 7, 7);
+		CURSOR_N = new Cursor(display, new ImageData(MoveResize.class.getResourceAsStream("CURSOR_N.png")), 7, 0);
+		CURSOR_E = new Cursor(display, new ImageData(MoveResize.class.getResourceAsStream("CURSOR_E.png")), 15, 7);
+		CURSOR_W = new Cursor(display, new ImageData(MoveResize.class.getResourceAsStream("CURSOR_W.png")), 0, 7);
+		CURSOR_S = new Cursor(display, new ImageData(MoveResize.class.getResourceAsStream("CURSOR_S.png")), 7, 15);
+		CURSOR_NE = new Cursor(display, new ImageData(MoveResize.class.getResourceAsStream("CURSOR_NE.png")), 15, 0);
+		CURSOR_NW = new Cursor(display, new ImageData(MoveResize.class.getResourceAsStream("CURSOR_NW.png")), 0, 0);
+		CURSOR_SE = new Cursor(display, new ImageData(MoveResize.class.getResourceAsStream("CURSOR_SE.png")), 15, 15);
+		CURSOR_SW = new Cursor(display, new ImageData(MoveResize.class.getResourceAsStream("CURSOR_SW.png")), 0, 15);
 	}
 	
 	private static class ImagePainter implements PaintListener {
