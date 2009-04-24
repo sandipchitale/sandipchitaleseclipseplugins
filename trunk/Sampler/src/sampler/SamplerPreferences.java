bf45c98bac53ce85db5aeb5247786c79b85937c8
sandipchitale <sandipchitale@47c47cea-daa5-11dd-801b-4930de48a34e>
2009-04-24 03:35:13 +0000

    Remember current format.

--- trunk/Sampler/src/sampler/SamplerPreferences.java
+++ trunk/Sampler/src/sampler/SamplerPreferences.java
@@ -1,24 +1,38 @@
 package sampler;
 
+import java.util.LinkedList;
+import java.util.List;
 import java.util.regex.Pattern;
 
 import org.eclipse.core.runtime.preferences.AbstractPreferenceInitializer;
 import org.eclipse.jface.preference.IPreferenceStore;
 
 public class SamplerPreferences extends AbstractPreferenceInitializer {
+	public static String LABLE_VALUE_SEPARATOR = "|||";
+
+	private static List<String> formatsList = new LinkedList<String>();
+	static {
+		formatsList.add("#RRGGBB (HTML/CSS)" + LABLE_VALUE_SEPARATOR + "#%1$02x%2$02x%3$02x");
+		formatsList.add("rgb(r,g,b) (CSS)" + LABLE_VALUE_SEPARATOR + "rgb(%1$d, %2$d, %3$d)");
+		formatsList.add("new Color(r,g,b) (AWT)" + LABLE_VALUE_SEPARATOR + "new Color(%1$3d, %2$3d, %3$3d)");
+	}
 
 	public static final String COPY_TO_CLIPBOARD = Activator.PLUGIN_ID + ".COPY_TO_CLIPBOARD";
 	public static final String CUSTOM_FORMATS = Activator.PLUGIN_ID + ".CUSTOM_FORMATS";
+	public static final String CURRENT_FORMAT = Activator.PLUGIN_ID + ".CURRENT_FORMAT";
 
 	public SamplerPreferences() {
 	}
 
 	public void initializeDefaultPreferences() {
 		IPreferenceStore prefs = Activator.getDefault().getPreferenceStore();
-		prefs.setDefault(CUSTOM_FORMATS, "");
+		String[] formatsArray = formatsList.toArray(new String[0]);
+		String formats = createList(formatsArray);
+		prefs.setDefault(CUSTOM_FORMATS, formats);
+		prefs.setDefault(CURRENT_FORMAT, formatsArray[0]);
 		prefs.setDefault(COPY_TO_CLIPBOARD, true);
 	}
-	
+
 	public static void setCopyToClipboard(boolean copyToClipboard) {
 		IPreferenceStore prefs = Activator.getDefault().getPreferenceStore();
 		prefs.setValue(COPY_TO_CLIPBOARD, copyToClipboard);
@@ -32,6 +46,25 @@ public class SamplerPreferences extends AbstractPreferenceInitializer {
 	public static String[] getCustomFormats() {
 		IPreferenceStore prefs = Activator.getDefault().getPreferenceStore();
 		return parseString(prefs.getString(CUSTOM_FORMATS));
+	}
+	
+	public static String getCurrentFormat() {
+		IPreferenceStore prefs = Activator.getDefault().getPreferenceStore();
+		String currentFormat = prefs.getString(CURRENT_FORMAT);
+		if ("".equals(currentFormat)) {
+			String[] customFormats = getCustomFormats();
+			if (customFormats.length > 0) {
+				currentFormat = customFormats[0];
+			} else {
+				currentFormat = "Red,Green,BLUE"  + SamplerPreferences.LABLE_VALUE_SEPARATOR + "{r}, {g}, {b}";
+			}
+		}
+		return currentFormat;
+	}
+	
+	public static void setCurrentFormat(String currentFormat) {
+		IPreferenceStore prefs = Activator.getDefault().getPreferenceStore();
+		prefs.setValue(CURRENT_FORMAT, currentFormat);
 	}
 	
 	private static String SEPARATOR = "@@@@";
