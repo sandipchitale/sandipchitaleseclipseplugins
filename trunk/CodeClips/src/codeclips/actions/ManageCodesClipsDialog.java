0d25ffe2c91848632b2b7acd13c9e0ea3b1ecc61
sandipchitale <sandipchitale@47c47cea-daa5-11dd-801b-4930de48a34e>
2009-11-14 06:51:04 +0000

    Add Manage Clips dialog.

--- trunk/CodeClips/src/codeclips/actions/ManageCodesClipsDialog.java
+++ trunk/CodeClips/src/codeclips/actions/ManageCodesClipsDialog.java
@@ -7,8 +7,6 @@ import org.eclipse.jface.text.templates.Template;
 import org.eclipse.jface.text.templates.persistence.TemplateStore;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.StyledText;
-import org.eclipse.swt.events.SelectionEvent;
-import org.eclipse.swt.events.SelectionListener;
 import org.eclipse.swt.events.VerifyEvent;
 import org.eclipse.swt.events.VerifyListener;
 import org.eclipse.swt.layout.GridData;
@@ -17,28 +15,26 @@ import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.texteditor.ITextEditor;
 
 import codeclips.Activator;
 
-public class CodeClipDialog extends TitleAreaDialog{
+public class ManageCodesClipsDialog extends TitleAreaDialog{
 
 	private String abbrev;
 	private String expansion;
 	private StyledText expansionText;
 	private final ITextEditor textEditor;
 	private Text abbrevText;
-	private Button manageButton;
-	private Button saveUpdateButton;
+	private Button newButton;
+	private Button modifyButton;
+	private Button deleteButton;
 	private TemplateStore templateStore;
 
-	public CodeClipDialog(ITextEditor textEditor) {
-		this(textEditor, "");
-	}
-
-	public CodeClipDialog(ITextEditor textEditor, String expansion) {
-		super(textEditor.getSite().getShell());
+	public ManageCodesClipsDialog(Shell shell, ITextEditor textEditor) {
+		super(shell);
 		this.textEditor = textEditor;
 		abbrev = "";
 		this.expansion = expansion;
@@ -52,8 +48,7 @@ public class CodeClipDialog extends TitleAreaDialog{
 	
 	@Override
 	protected Control createDialogArea(Composite parent) {
-		setTitle("Code Clip");
-		setMessage("Create/Update a Code Clip");
+		setTitle("Manage Code Clips");
         Composite parentComposite = (Composite) super.createDialogArea(parent);
         
         GridLayout gridLayout = new GridLayout(1, false);
@@ -65,37 +60,9 @@ public class CodeClipDialog extends TitleAreaDialog{
 		gridLayout.verticalSpacing = 5;
         
         Label abbrevLabel = new Label(parentComposite, SWT.NONE);
-        abbrevLabel.setText("Clip Name:");
+        abbrevLabel.setText("Clips:");
 		GridData abbrevLabelGridData = new GridData(SWT.LEFT, SWT.CENTER, false, false);
 		abbrevLabel.setLayoutData(abbrevLabelGridData);
-		
-        abbrevText = new Text(parentComposite, SWT.SINGLE | SWT.BORDER);
-        abbrevText.setText(abbrev);
-		GridData abbrevTextGridData = new GridData(SWT.FILL, SWT.CENTER, true, false);
-		abbrevText.setLayoutData(abbrevTextGridData);
-		
-		abbrevText.addVerifyListener(new VerifyListener() {
-			public void verifyText(VerifyEvent e) {
-				Template existingTemplate = templateStore.findTemplate(abbrevText.getText());
-				if (existingTemplate == null) {
-					saveUpdateButton.setText("Update");					
-				} else {
-					saveUpdateButton.setText("Save");
-				}
-			}
-		});
-        
-        Label expansionLabel = new Label(parentComposite, SWT.NONE);
-        expansionLabel.setText("Clip Text:");
-		GridData expansionLabelGridData = new GridData(SWT.LEFT, SWT.CENTER, false, false);
-		expansionLabel.setLayoutData(expansionLabelGridData);
-		
-		expansionText = new StyledText(parentComposite, SWT.MULTI | SWT.BORDER | SWT.H_SCROLL | SWT.V_SCROLL);
-		expansionText.setText(expansion);
-		expansionText.setFont(JFaceResources.getFontRegistry().get(JFaceResources.TEXT_FONT));
-		GridData styledTextGridData = new GridData(SWT.FILL, SWT.FILL, true, true);
-		styledTextGridData.heightHint = expansionText.getLineHeight() * 8;
-		expansionText.setLayoutData(styledTextGridData);
 		
 		return parentComposite;
 	}
@@ -117,18 +84,10 @@ public class CodeClipDialog extends TitleAreaDialog{
 	
 	@Override
 	protected void createButtonsForButtonBar(Composite parent) {
-		manageButton = createButton(parent, IDialogConstants.CLIENT_ID, "Manage...", false);
-		manageButton.addSelectionListener(new SelectionListener() {
-			public void widgetSelected(SelectionEvent e) {
-				new ManageCodesClipsDialog(getShell(), textEditor).open();
-			}
-			
-			public void widgetDefaultSelected(SelectionEvent e) {
-				widgetSelected(e);
-			}
-		});
-		createButton(parent, IDialogConstants.CANCEL_ID, IDialogConstants.CANCEL_LABEL, false);
-		saveUpdateButton = createButton(parent, IDialogConstants.OK_ID, "Save", true);
+		newButton = createButton(parent, IDialogConstants.CLIENT_ID, "Add...", false);
+		modifyButton = createButton(parent, IDialogConstants.CLIENT_ID+1, "Modify...", true);
+		deleteButton = createButton(parent, IDialogConstants.CLIENT_ID+2, "Delete", true);
+		createButton(parent, IDialogConstants.CANCEL_ID, IDialogConstants.CLOSE_LABEL, false);
 	}
 	
 	void setExpansion(String expansion) {
