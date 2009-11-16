734189500a75fa2df0d25501f28becf8c6192c8f
sandipchitale <sandipchitale@47c47cea-daa5-11dd-801b-4930de48a34e>
2009-11-16 05:47:57 +0000

    Fix warnings.

--- trunk/CodeClips/src/codeclips/templates/TabCompleteCodeClipsCommandHandler.java
+++ trunk/CodeClips/src/codeclips/templates/TabCompleteCodeClipsCommandHandler.java
@@ -5,7 +5,6 @@ import java.util.concurrent.atomic.AtomicBoolean;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
-import org.eclipse.core.expressions.IEvaluationContext;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
