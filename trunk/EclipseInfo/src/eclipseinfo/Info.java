d7f501dc613d9d395b194fb23d5c93456cbe2fde
sandipchitale <sandipchitale@47c47cea-daa5-11dd-801b-4930de48a34e>
2009-11-18 16:46:06 +0000

    PVE

--- trunk/EclipseInfo/src/eclipseinfo/Info.java
+++ trunk/EclipseInfo/src/eclipseinfo/Info.java
@@ -10,8 +10,16 @@ import org.eclipse.core.internal.content.ContentTypeManager;
 import org.eclipse.core.runtime.content.IContentType;
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.ui.IEditorDescriptor;
+import org.eclipse.ui.IEditorRegistry;
+import org.eclipse.ui.IFileEditorMapping;
+import org.eclipse.ui.IPerspectiveDescriptor;
+import org.eclipse.ui.IPerspectiveRegistry;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.IWorkbenchWindowActionDelegate;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.views.IViewDescriptor;
+import org.eclipse.ui.views.IViewRegistry;
 
 public class Info implements IWorkbenchWindowActionDelegate {
 
@@ -32,8 +40,12 @@ public class Info implements IWorkbenchWindowActionDelegate {
 		// Extensions		
 		
 		// Perspectives
+		perspectives(out);
 		// Views
+		views(out);
 		// Editors
+		editors(out);
+		
 		contentTypes(out);
 		out.flush(); 
 	}
@@ -41,6 +53,92 @@ public class Info implements IWorkbenchWindowActionDelegate {
 	public void selectionChanged(IAction action, ISelection selection) {
 	}
 
+	private static void perspectives(PrintWriter out) {
+		String header = "Perspectives";
+		header(out, header);
+		IPerspectiveRegistry perspectiveRegistry = PlatformUI.getWorkbench().getPerspectiveRegistry();
+		IPerspectiveDescriptor[] perspectives = perspectiveRegistry.getPerspectives();
+		SortedSet<IPerspectiveDescriptor> sortedPerspectiveDescriptors = new TreeSet<IPerspectiveDescriptor>(
+				new Comparator<IPerspectiveDescriptor>() {
+					public int compare(IPerspectiveDescriptor o1, IPerspectiveDescriptor o2) {
+						return o1.getId().compareTo(o2.getId());
+					}
+				}
+				);
+		sortedPerspectiveDescriptors.addAll(Arrays.asList(perspectives));
+		
+		for (IPerspectiveDescriptor perspectiveDescriptor : sortedPerspectiveDescriptors) {
+			out.println(
+					perspectiveDescriptor.getId()
+					+ " "
+					+ perspectiveDescriptor.getLabel()
+					+ " "
+					+ perspectiveDescriptor.getDescription()
+					);
+		}
+
+		footer(out, header);
+	}
+
+	private static void views(PrintWriter out) {
+		String header = "Views";
+		header(out, header);
+		IViewRegistry viewRegistry = PlatformUI.getWorkbench().getViewRegistry();
+		IViewDescriptor[] viewDescriptors = viewRegistry.getViews();
+		SortedSet<IViewDescriptor> sortedViewDescriptors = new TreeSet<IViewDescriptor>(
+				new Comparator<IViewDescriptor>() {
+					public int compare(IViewDescriptor o1, IViewDescriptor o2) {
+						return o1.getId().compareTo(o2.getId());
+					}
+				}
+				);
+		sortedViewDescriptors.addAll(Arrays.asList(viewDescriptors));
+		
+		for (IViewDescriptor viewDescriptor : sortedViewDescriptors) {
+			out.println(
+					viewDescriptor.getId()
+					+ " "
+					+ viewDescriptor.getLabel()
+					+ " "
+					+ viewDescriptor.getDescription()
+					);
+		}
+
+		footer(out, header);
+	}
+	
+	private static void editors(PrintWriter out) {
+		String header = "Editors";
+		header(out, header);
+		IEditorRegistry editorRegistry = PlatformUI.getWorkbench().getEditorRegistry();
+		IFileEditorMapping[] fileEditorMappings = editorRegistry.getFileEditorMappings();
+		for (IFileEditorMapping fileEditorMapping : fileEditorMappings) {
+			IEditorDescriptor[] editorDescriptors = fileEditorMapping.getEditors();
+			SortedSet<IEditorDescriptor> sortedContentTypes = new TreeSet<IEditorDescriptor>(
+					new Comparator<IEditorDescriptor>() {
+						public int compare(IEditorDescriptor o1, IEditorDescriptor o2) {
+							return o1.getId().compareTo(o2.getId());
+						}
+					}
+					);
+			sortedContentTypes.addAll(Arrays.asList(editorDescriptors));
+			
+			for (IEditorDescriptor editorDescriptor : sortedContentTypes) {
+				out.println(
+						fileEditorMapping.getExtension()
+						+ " "
+						+ fileEditorMapping.getLabel()
+						+ " "						
+						+ editorDescriptor.getId()
+						+ " "
+						+ editorDescriptor.getLabel()
+						);
+			}
+		}
+
+		footer(out, header);
+	}
+	
 	private static void contentTypes(PrintWriter out) {
 		String header = "Content Types";
 		header(out, header);
