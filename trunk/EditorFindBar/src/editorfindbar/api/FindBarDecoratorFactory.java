583a18f25b36dad0e93c95e99de3382437a3b18c
sandipchitale <sandipchitale@47c47cea-daa5-11dd-801b-4930de48a34e>
2009-11-22 21:06:17 +0000

    Formalize creation of the action. Better example in JavaDoc.

--- trunk/EditorFindBar/src/editorfindbar/api/FindBarDecoratorFactory.java
+++ trunk/EditorFindBar/src/editorfindbar/api/FindBarDecoratorFactory.java
@@ -9,16 +9,24 @@ import editorfindbar.impl.FindBarDecorator;
  * This is a factory for FindBarDecorator.
  * <p>
  * The typical usage is as follows:
- * <code>
+ * <pre>
+ * public void createPartControl(Composite parent) {
+ *     Composite findBarComposite = getfindBarDecorator().createFindBarComposite(parent);
+ *     super.createPartControl(findBarComposite);
+ *     getfindBarDecorator().createFindBar(getSourceViewer());
+ * }
  * 
- * 	public void createPartControl(Composite parent) {
- *      IFindBarDecorator findBarDecorator = 
- *          FindBarDecoratorFactory.createBarDecorator(this, getStatusLineManager());
- *      Composite findBarComposite = findBarDecorator.createFindBarComposite(parent);
- *      super.createPartControl(parent);
- *      findBarComposite.createFindBar(getSourceViewer());
- *  }
- * </code>
+ * protected void createActions() {
+ *     super.createActions();
+ *     getfindBarDecorator().createActions();
+ * }
+ * 
+ * private AtomicReference<IFindBarDecorator> findBarDecorator = new AtomicReference<IFindBarDecorator>();
+ * private IFindBarDecorator getfindBarDecorator() {
+ *     findBarDecorator.compareAndSet(null, FindBarDecoratorFactory.createFindBarDecorator(this, getStatusLineManager()));
+ *     return findBarDecorator.get();
+ * }
+ * </pre>
  * @author schitale
  *
  */
