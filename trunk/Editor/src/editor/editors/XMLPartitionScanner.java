cbbc0d138e226e63d4233e2da1740bfcc5b560b1
sandipchitale <sandipchitale@47c47cea-daa5-11dd-801b-4930de48a34e>
2009-11-23 20:39:21 +0000

    Organized imports.

--- trunk/Editor/src/editor/editors/XMLPartitionScanner.java
+++ trunk/Editor/src/editor/editors/XMLPartitionScanner.java
@@ -1,6 +1,10 @@
 package editor.editors;
 
-import org.eclipse.jface.text.rules.*;
+import org.eclipse.jface.text.rules.IPredicateRule;
+import org.eclipse.jface.text.rules.IToken;
+import org.eclipse.jface.text.rules.MultiLineRule;
+import org.eclipse.jface.text.rules.RuleBasedPartitionScanner;
+import org.eclipse.jface.text.rules.Token;
 
 public class XMLPartitionScanner extends RuleBasedPartitionScanner {
 	public final static String XML_COMMENT = "__xml_comment";
