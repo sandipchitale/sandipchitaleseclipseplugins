f4727791c805edb58209c9ff8ce1728497c5936b
sandipchitale <sandipchitale@47c47cea-daa5-11dd-801b-4930de48a34e>
2009-11-29 17:43:00 +0000

    Initial check in.

---
+++ trunk/commandkeybinding.xref/src/commandkeybinding/xref/ShowCommandKeybindingXREF.java
@@ -0,0 +1,26 @@
+package commandkeybinding.xref;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+
+public class ShowCommandKeybindingXREF extends AbstractHandler {
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		CommandKeybindingXREFDialog commandKeybindingXREFDialog = new CommandKeybindingXREFDialog();
+		commandKeybindingXREFDialog.open();
+		return null;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
+
+	@Override
+	public boolean isHandled() {
+		return true;
+	}
+
+}
