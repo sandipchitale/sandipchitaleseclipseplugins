caa46d76a195b690e3b6bfeffa4bf2292bc87177
sandipchitale <sandipchitale@47c47cea-daa5-11dd-801b-4930de48a34e>
2011-04-21 03:36:06 +0000

    Handle the text editor inside a MultiPageEditor.

--- FindReplaceBar/src/findreplacebar/ShowFindReplaceBarHandler.java
+++ FindReplaceBar/src/findreplacebar/ShowFindReplaceBarHandler.java
@@ -6,14 +6,16 @@ import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.commands.NotEnabledException;
 import org.eclipse.core.commands.NotHandledException;
 import org.eclipse.core.commands.common.NotDefinedException;
+import org.eclipse.jface.text.IFindReplaceTarget;
+import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.IViewPart;
 import org.eclipse.ui.IWorkbenchCommandConstants;
 import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.IWorkbenchPart;
+import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.handlers.HandlerUtil;
 import org.eclipse.ui.handlers.IHandlerService;
-import org.eclipse.ui.texteditor.AbstractTextEditor;
-import org.eclipse.ui.texteditor.ITextEditor;
 
 /**
  * This shows the Find Replace Bar view.
@@ -24,30 +26,42 @@ import org.eclipse.ui.texteditor.ITextEditor;
 public class ShowFindReplaceBarHandler extends AbstractHandler {
 	public Object execute(ExecutionEvent event) throws ExecutionException {
 		Object variable = HandlerUtil.getActivePart(event);
-		if (variable instanceof ITextEditor) {
-			ITextEditor textEditor = (ITextEditor) variable;
-			if (Activator.getDefault().getOverrideFindReplaceDialog() && textEditor instanceof AbstractTextEditor) {
-				IWorkbenchPage activePage = textEditor.getSite().getWorkbenchWindow().getActivePage();
-				IViewPart viewPart;
-				try {
-					viewPart = activePage.showView("FindReplaceBar.view");
-					activePage.activate(viewPart);
-					((FindReplaceBarViewPart)viewPart).startFind();
-				} catch (PartInitException e) {
-				}
-			} else {
-				IHandlerService handlerService = (IHandlerService) textEditor
-						.getSite().getService(IHandlerService.class);
-				try {
-					handlerService.executeCommand(
-							IWorkbenchCommandConstants.EDIT_FIND_AND_REPLACE,
-							null);
-				} catch (NotDefinedException e) {
-				} catch (NotEnabledException e) {
-				} catch (NotHandledException e) {
+		
+		if (variable instanceof IWorkbenchPart) {
+			IWorkbenchPart workbenchPart = (IWorkbenchPart) variable;
+			Object adapter = workbenchPart.getAdapter(IFindReplaceTarget.class);
+			if (adapter != null) {
+				if (Activator.getDefault().getOverrideFindReplaceDialog() && workbenchPart instanceof IEditorPart) {
+					showFindReplaceBar(workbenchPart.getSite().getWorkbenchWindow());
+				} else {
+					showFindReplaceDialog(workbenchPart);
 				}
 			}
 		}
+		
 		return null;
+	}
+	private static void showFindReplaceBar(IWorkbenchWindow workbenchWindow) {
+		IWorkbenchPage activePage = workbenchWindow.getActivePage();
+		IViewPart viewPart;
+		try {
+			viewPart = activePage.showView("FindReplaceBar.view");
+			activePage.activate(viewPart);
+			((FindReplaceBarViewPart)viewPart).startFind();
+		} catch (PartInitException e) {
+		}
+	}
+	
+	private static void showFindReplaceDialog(IWorkbenchPart workbenchPart) {
+		IHandlerService handlerService = (IHandlerService) workbenchPart.getSite().getService(IHandlerService.class);
+		try {
+			handlerService.executeCommand(
+					IWorkbenchCommandConstants.EDIT_FIND_AND_REPLACE,
+					null);
+		} catch (NotDefinedException e) {
+		} catch (NotEnabledException e) {
+		} catch (NotHandledException e) {
+		} catch (ExecutionException e) {
+		}
 	}
 }
