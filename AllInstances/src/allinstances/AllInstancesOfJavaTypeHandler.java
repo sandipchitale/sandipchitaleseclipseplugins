fbb5045dae0ef65b047d598918a71f905fbe9355
sandipchitale <sandipchitale@47c47cea-daa5-11dd-801b-4930de48a34e>
2011-05-30 22:53:36 +0000

    Also show instances of subclasses.

--- AllInstances/src/allinstances/AllInstancesOfJavaTypeHandler.java
+++ AllInstances/src/allinstances/AllInstancesOfJavaTypeHandler.java
@@ -1,5 +1,10 @@
 package allinstances;
 
+import java.util.LinkedHashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
@@ -12,6 +17,7 @@ import org.eclipse.debug.ui.IDebugUIConstants;
 import org.eclipse.jdt.core.IType;
 import org.eclipse.jdt.core.search.IJavaSearchConstants;
 import org.eclipse.jdt.debug.core.IJavaClassObject;
+import org.eclipse.jdt.debug.core.IJavaClassType;
 import org.eclipse.jdt.debug.core.IJavaDebugTarget;
 import org.eclipse.jdt.debug.core.IJavaFieldVariable;
 import org.eclipse.jdt.debug.core.IJavaObject;
@@ -19,6 +25,7 @@ import org.eclipse.jdt.debug.core.IJavaThread;
 import org.eclipse.jdt.debug.core.IJavaType;
 import org.eclipse.jdt.debug.core.IJavaValue;
 import org.eclipse.jdt.internal.debug.core.logicalstructures.JDIAllInstancesValue;
+import org.eclipse.jdt.internal.debug.core.model.JDIClassObjectValue;
 import org.eclipse.jdt.internal.debug.core.model.JDIClassType;
 import org.eclipse.jdt.internal.debug.core.model.JDIDebugTarget;
 import org.eclipse.jdt.internal.debug.ui.IJDIPreferencesConstants;
@@ -81,82 +88,126 @@ public class AllInstancesOfJavaTypeHandler extends AbstractHandler {
 					IType iType = (IType) typesArray[0];
 					IJavaType[] types;
 					try {
+						List<IJavaClassObject> allClassObjects = new LinkedList<IJavaClassObject>();
+						IJavaType[] javaLangClasses = target.getJavaTypes(Class.class.getName());
+						for (IJavaType javaLangClassType : javaLangClasses) {
+							if (javaLangClassType instanceof JDIClassType) {
+								JDIClassType javaLangClass = (JDIClassType) javaLangClassType;
+								IJavaObject[] javaLangClassInstances = javaLangClass.getInstances(Long.MAX_VALUE);
+								for (IJavaObject javaLangClassInstance : javaLangClassInstances) {
+									if (javaLangClassInstance instanceof IJavaClassObject) {
+										IJavaClassObject javaLangClassInstanceClassObject = (IJavaClassObject) javaLangClassInstance;
+										allClassObjects.add(javaLangClassInstanceClassObject);
+									}
+								}
+							}
+						}
+						
 						types = target.getJavaTypes(iType.getFullyQualifiedName());
 						if (types == null || types.length == 0) {
 							// If the type is not known the VM, open
 							// a pop-up dialog with 0 instances
-							MessageDialog.openError(shell, "No Instances", iType.getFullyQualifiedName());
+							MessageDialog.openError(shell, "No Class", "No Class with name: " + iType.getFullyQualifiedName());
 							return null;
 						}
 						boolean activateExpressionsView = false;
 						for (IJavaType type : types) {
 							if (type instanceof JDIClassType) {
 								JDIClassType classType = (JDIClassType) type;
-								IJavaClassObject classObject = classType.getClassObject();
-								try {
-
-									JDIAllInstancesValue aiv = new JDIAllInstancesValue(
-											(JDIDebugTarget) classType.getDebugTarget(), classType);
-									DebugPlugin
-											.getDefault()
-											.getExpressionManager()
-											.addExpression(
-													new JavaInspectExpression(classType.getName() + " Instances (" + classType.getInstanceCount() + ")", aiv));
-
+								Set<JDIClassType> typeAndSubTypes = new LinkedHashSet<JDIClassType>();
+								typeAndSubTypes.add(classType);
+								if (!Object.class.getName().equals(classType.getName())) {
+									for (IJavaClassObject allClassObject : allClassObjects) {
+										IJavaType instanceType = allClassObject.getInstanceType();
+										if (instanceType instanceof JDIClassType) {
+											JDIClassType jdiClassTypeSaved = (JDIClassType) instanceType;
+											JDIClassType jdiClassType = jdiClassTypeSaved;
+											boolean isSubClass = false;
+											do {
+												if (typeAndSubTypes.contains(jdiClassType)) {
+													isSubClass = true;
+													break;
+												}
+												jdiClassType = (JDIClassType)jdiClassType.getSuperclass();
+											} while (jdiClassType != null);
+											if (isSubClass) {
+												typeAndSubTypes.add(jdiClassTypeSaved);
+											}
+										}										
+									}
+								}
+								
+								// TODO Topological sort - superclasses before subclasses
+								
+								for (JDIClassType typeOrSubType : typeAndSubTypes) {
+									IJavaClassObject classObject = typeOrSubType.getClassObject();
 									try {
-										IThread suspendedThread = null;
-										IThread[] threads = target.getThreads();
-										for (IThread thread : threads) {
-											if (thread.isSuspended()) {
-												suspendedThread = thread;
-												break;
-											}
-										}
 
-										if (suspendedThread != null) {
-											IJavaValue javaValue = classObject.sendMessage("getProtectionDomain", "()Ljava/security/ProtectionDomain;", null, (IJavaThread) suspendedThread, null);
-											if (javaValue instanceof IJavaObject) {
-												javaValue = ((IJavaObject) javaValue).sendMessage("getCodeSource", "()Ljava/security/CodeSource;", null, (IJavaThread) suspendedThread, null);
+										JDIAllInstancesValue aiv = new JDIAllInstancesValue(
+												(JDIDebugTarget) typeOrSubType.getDebugTarget(), typeOrSubType);
+										DebugPlugin
+												.getDefault()
+												.getExpressionManager()
+												.addExpression(
+														new JavaInspectExpression(typeOrSubType.getName() + " Instances (" + typeOrSubType.getInstanceCount() + ")", aiv));
+										activateExpressionsView = true;
+
+										try {
+											IThread suspendedThread = null;
+											IThread[] threads = target.getThreads();
+											for (IThread thread : threads) {
+												if (thread.isSuspended()) {
+													suspendedThread = thread;
+													break;
+												}
+											}
+
+											if (suspendedThread != null) {
+												IJavaValue javaValue = classObject.sendMessage("getProtectionDomain", "()Ljava/security/ProtectionDomain;", null, (IJavaThread) suspendedThread, null);
 												if (javaValue instanceof IJavaObject) {
-													IJavaObject classLoaderObject = classType.getClassLoaderObject();
-													DebugPlugin
-													.getDefault()
-													.getExpressionManager()
-													.addExpression(
-															new JavaInspectExpression(
-																	classType.getName() + " CodeSource" + (classLoaderObject == null ? "" : "(ClassLoader " + classLoaderObject + ")"),
-																	javaValue));
+													javaValue = ((IJavaObject) javaValue).sendMessage("getCodeSource", "()Ljava/security/CodeSource;", null, (IJavaThread) suspendedThread, null);
+													if (javaValue instanceof IJavaObject) {
+														IJavaObject classLoaderObject = typeOrSubType.getClassLoaderObject();
+														DebugPlugin
+														.getDefault()
+														.getExpressionManager()
+														.addExpression(
+																new JavaInspectExpression(
+																		typeOrSubType.getName() + " CodeSource" + (classLoaderObject == null ? "" : "(ClassLoader " + classLoaderObject + ")"),
+																		javaValue));
+														activateExpressionsView = true;
+													}
+												}
+											}
+										} catch (Exception e) {
+										}
+
+										// If no instances available - at least show the static fields
+										if (aiv.getValues().length == 0) {
+											IPreferenceStore preferenceStore = JDIDebugUIPlugin.getDefault().getPreferenceStore();
+											if (preferenceStore.getBoolean(IDebugUIConstants.ID_EXPRESSION_VIEW + "." + IJDIPreferencesConstants.PREF_SHOW_STATIC_VARIABLES)) {
+												// Static fields
+												String[] declaredFieldNames = typeOrSubType.getDeclaredFieldNames();
+												for (String fieldName : declaredFieldNames) {
+													IJavaFieldVariable field = typeOrSubType.getField(fieldName);
+													if (field != null && field.isStatic()) {
+														DebugPlugin
+														.getDefault()
+														.getExpressionManager()
+														.addExpression(
+																new JavaInspectExpression(
+																		typeOrSubType.getName() + "." + field.getName(),
+																		(IJavaValue) field.getValue()));
+														activateExpressionsView = true;
+													}
 												}
 											}
 										}
-									} catch (Exception e) {
-									}
 
-									// If no instances available - at least show the static fields
-									if (aiv.getValues().length == 0) {
-										IPreferenceStore preferenceStore = JDIDebugUIPlugin.getDefault().getPreferenceStore();
-										if (preferenceStore.getBoolean(IDebugUIConstants.ID_EXPRESSION_VIEW + "." + IJDIPreferencesConstants.PREF_SHOW_STATIC_VARIABLES)) {
-											// Static fields
-											String[] declaredFieldNames = classType.getDeclaredFieldNames();
-											for (String fieldName : declaredFieldNames) {
-												IJavaFieldVariable field = classType.getField(fieldName);
-												if (field != null && field.isStatic()) {
-													DebugPlugin
-													.getDefault()
-													.getExpressionManager()
-													.addExpression(
-															new JavaInspectExpression(
-																	classType.getName() + "." + field.getName(),
-																	(IJavaValue) field.getValue()));
-												}
-											}
-										}
-									}
-
-									activateExpressionsView = true;
-								} catch (DebugException e) {
-									MessageDialog.openError(shell, "Exception",
-											"Exception while showing all instances of :" + iType.getElementName());
+									} catch (DebugException e) {
+										MessageDialog.openError(shell, "Exception",
+												"Exception while showing all instances of :" + iType.getElementName());
+									}									
 								}
 							}
 						}
