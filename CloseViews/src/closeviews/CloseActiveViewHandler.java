5ffb9c279f9a1e1940fde79b37848d36fc4f55c6
sandipchitale <sandipchitale@47c47cea-daa5-11dd-801b-4930de48a34e>
2011-12-01 10:07:22 +0000

    Better behavior of Close View command. Basically repeated invocation closes most recently focused view.

--- CloseViews/src/closeviews/CloseActiveViewHandler.java
+++ CloseViews/src/closeviews/CloseActiveViewHandler.java
@@ -3,22 +3,56 @@ package closeviews;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.ui.IViewPart;
+import org.eclipse.ui.IViewReference;
 import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.IWorkbenchPart;
+import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.handlers.HandlerUtil;
 
 public class CloseActiveViewHandler extends AbstractHandler {
 
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
-		IWorkbenchPart activePart = HandlerUtil.getActivePart(event);
-		if (activePart instanceof IViewPart) {
-			IViewPart viewPart = (IViewPart) activePart;
-			IWorkbenchPage activePage = viewPart.getSite().getWorkbenchWindow()
+		IWorkbenchWindow activeWorkbenchWindow = HandlerUtil
+				.getActiveWorkbenchWindow(event);
+		if (activeWorkbenchWindow != null) {
+			IWorkbenchPage activePage = activeWorkbenchWindow
 					.getActivePage();
-			if (activePage != null) {
-				activePage.hideView(viewPart);
+			IWorkbenchPart activePart = HandlerUtil.getActivePart(event);
+			if (activePart instanceof IViewPart) {
+				IViewPart viewPart = (IViewPart) activePart;
+				if (activePage != null) {
+					if (activePage.isPageZoomed()) {
+						activePage.zoomOut();
+						if (!MessageDialog.openConfirm(
+								activeWorkbenchWindow.getShell(),
+								"Confirm Close Next View", "Close Next View?")) {
+							return null;
+						}
+					}
+					activePage.hideView(viewPart);
+				}
+			} else {
+				if (activePage != null) {
+					IViewReference[] viewReferences = activePage
+							.getViewReferences();
+					if (viewReferences.length > 0) {
+						if (activePage.isPageZoomed()) {
+							activePage.zoomOut();
+							if (!MessageDialog.openConfirm(
+									activeWorkbenchWindow.getShell(),
+									"Confirm Close Next View",
+									"Close Next View?")) {
+								return null;
+							}
+						}
+						activePage.hideView(viewReferences[0]);
+					} else {
+						activeWorkbenchWindow.getShell().getDisplay().beep();
+					}
+				}
 			}
 		}
 		return null;
